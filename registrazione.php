21ae5a174d2b924ca5566d434c4a4aab281d8223
scicolone <53647201+scicolone@users.noreply.github.com>
2025-08-24 18:39:52 +0200

    Update registrazione.php

--- registrazione.php
+++ registrazione.php
@@ -91,73 +91,83 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <?php if ($error): ?>
         <div class="error"><?= htmlspecialchars($error) ?></div>
     <?php elseif ($success): ?>
-        <div class="success"><?= htmlspecialchars($success) ?></div>
-    <?php endif; ?>
-
-    <form method="post" autocomplete="off">
-        <label>Nome</label>
-        <input type="text" name="nome" required>
-
-        <label>Cognome</label>
-        <input type="text" name="cognome" required>
-
-        <label>Luogo di nascita</label>
-        <input type="text" name="luogo_nascita" required>
-
-        <label>Provincia di nascita</label>
-        <input type="text" name="provincia_nascita" maxlength="2" required>
-
-        <label>Data di nascita</label>
-        <input type="date" name="data_nascita" required>
-
-        <label>Codice fiscale</label>
-        <input type="text" name="codice_fiscale" maxlength="16" required pattern="[A-Za-z0-9]{16}">
-
-        <label>Indirizzo</label>
-        <input type="text" name="indirizzo" required>
-
-        <label>Comune di residenza</label>
-        <input type="text" name="comune_residenza" required>
-
-        <label>Provincia di residenza</label>
-        <input type="text" name="provincia_residenza" maxlength="2" required>
-
-        <label>Email</label>
-        <input type="email" name="email" required>
-
-        <label>Password</label>
-        <input type="password" name="password" minlength="6" required>
-
-        <label>Seleziona ruolo</label>
-        <div class="ruoli">
-            <?php
-            $ruoli = ['genitore','allenatore','socio','dirigente','segretario','cassiere'];
-            $icons = [
-                'genitore'  => '<path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"/>',
-                'allenatore'=> '<path d="M5 13.18v2.81c0 .73.4 1.41 1.04 1.76l5 2.5c.6.3 1.32.3 1.92 0l5-2.5c.64-.35 1.04-1.03 1.04-1.76v-2.81l-6 2.4-6-2.4zM12 3L1 9l11 6 9-4.5V17h2V9L12 3z"/>',
-                'socio'     => '<path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-.01L12 2z"/>',
-                'dirigente' => '<path d="M5 9.2h3V19H5zM10.6 5h2.8v14h-2.8zm5.6 8H19v6h-2.8z"/>',
-                'segretario'=> '<path d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-5 14H7v-2h7v2zm3-4H7v-2h10v2zm0-4H7V7h10v2z"/>',
-                'cassiere'  => '<path d="M11.8 10.9c-2.27-.59-3-1.2-3-2.15 0-1.09 1.01-1.85 2.7-1.85 1.78 0 2.44.85 2.5 2.1h2.21c-.07-1.72-1.12-3.3-3.21-3.81V3h-3v2.16c-1.94.42-3.5 1.68-3.5 3.61 0 2.31 1.91 3.46 4.7 4.13 2.5.6 3 1.48 3 2.41 0 .69-.49 1.79-2.7 1.79-2.06 0-2.87-.92-2.98-2.1h-2.2c.12 2.19 1.76 3.42 3.68 3.83V21h3v-2.15c1.95-.37 3.5-1.5 3.5-3.55 0-2.84-2.43-3.81-4.7-4.4z"/>'
-            ];
-            foreach ($ruoli as $r) {
-                $id = 'r_'.$r;
-                echo "
-                <div class='ruolo-item'>
-                    <input type='radio' name='ruolo' value='$r' id='$id' required>
-                    <label for='$id'>
-                        <svg viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
-                            {$icons[$r]}
-                        </svg>
-                        ".ucfirst($r)."
-                    </label>
-                </div>";
-            }
-            ?>
+        <div class="success">
+            <?= htmlspecialchars($success) ?>
+            <br><br>
+            <small>Ti stiamo reindirizzando alla Home…</small>
         </div>
+        <script>
+            setTimeout(function () {
+                window.location.href = 'index.php';
+            }, 7000);
+        </script>
+    <?php else: ?>
+        <!-- FORM COMPLETO -->
+        <form method="post" autocomplete="off">
+            <label>Nome</label>
+            <input type="text" name="nome" required>
 
-        <button type="submit">Registrati</button>
-    </form>
+            <label>Cognome</label>
+            <input type="text" name="cognome" required>
+
+            <label>Luogo di nascita</label>
+            <input type="text" name="luogo_nascita" required>
+
+            <label>Provincia di nascita</label>
+            <input type="text" name="provincia_nascita" maxlength="2" required>
+
+            <label>Data di nascita</label>
+            <input type="date" name="data_nascita" required>
+
+            <label>Codice fiscale</label>
+            <input type="text" name="codice_fiscale" maxlength="16" required pattern="[A-Za-z0-9]{16}">
+
+            <label>Indirizzo</label>
+            <input type="text" name="indirizzo" required>
+
+            <label>Comune di residenza</label>
+            <input type="text" name="comune_residenza" required>
+
+            <label>Provincia di residenza</label>
+            <input type="text" name="provincia_residenza" maxlength="2" required>
+
+            <label>Email</label>
+            <input type="email" name="email" required>
+
+            <label>Password</label>
+            <input type="password" name="password" minlength="6" required>
+
+            <label>Seleziona ruolo</label>
+            <div class="ruoli">
+                <?php
+                $ruoli = ['genitore','allenatore','socio','dirigente','segretario','cassiere'];
+                $icons = [
+                    'genitore'  => '<path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"/>',
+                    'allenatore'=> '<path d="M5 13.18v2.81c0 .73.4 1.41 1.04 1.76l5 2.5c.6.3 1.32.3 1.92 0l5-2.5c.64-.35 1.04-1.03 1.04-1.76v-2.81l-6 2.4-6-2.4zM12 3L1 9l11 6 9-4.5V17h2V9L12 3z"/>',
+                    'socio'     => '<path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-.01L12 2z"/>',
+                    'dirigente' => '<path d="M5 9.2h3V19H5zM10.6 5h2.8v14h-2.8zm5.6 8H19v6h-2.8z"/>',
+                    'segretario'=> '<path d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-5 14H7v-2h7v2zm3-4H7v-2h10v2zm0-4H7V7h10v2z"/>',
+                    'cassiere'  => '<path d="M11.8 10.9c-2.27-.59-3-1.2-3-2.15 0-1.09 1.01-1.85 2.7-1.85 1.78 0 2.44.85 2.5 2.1h2.21c-.07-1.72-1.12-3.3-3.21-3.81V3h-3v2.16c-1.94.42-3.5 1.68-3.5 3.61 0 2.31 1.91 3.46 4.7 4.13 2.5.6 3 1.48 3 2.41 0 .69-.49 1.79-2.7 1.79-2.06 0-2.87-.92-2.98-2.1h-2.2c.12 2.19 1.76 3.42 3.68 3.83V21h3v-2.15c1.95-.37 3.5-1.5 3.5-3.55 0-2.84-2.43-3.81-4.7-4.4z"/>'
+                ];
+                foreach ($ruoli as $r) {
+                    $id = 'r_'.$r;
+                    echo "
+                    <div class='ruolo-item'>
+                        <input type='radio' name='ruolo' value='$r' id='$id' required>
+                        <label for='$id'>
+                            <svg viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
+                                {$icons[$r]}
+                            </svg>
+                            ".ucfirst($r)."
+                        </label>
+                    </div>";
+                }
+                ?>
+            </div>
+
+            <button type="submit">Registrati</button>
+        </form>
+    <?php endif; ?>
 
     <a href="index.php" class="back">← Torna alla Home</a>
 </div>
